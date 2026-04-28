9b81d863f00d88cbf5ffe01c63e889adb2520994
Debora-Alves-Rodrigues <dtar@aluno.ifnmg.edu.br>
2026-04-28 17:45:11 +0000

    feat:Preenchendo a função buscarDados

--- exercicios/aula07/index.js
+++ exercicios/aula07/index.js
@@ -38,7 +38,8 @@ function simularConsulta(id) {
 
 // Exercício 4 — buscarDados
 export async function buscarDados(id) {
-  // escreva seu código aqui
+  const resultado = await simularConsulta(id);
+  return resultado;
 }
 
 // Exercício 5 — dividirSeguro
